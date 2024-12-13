c165d927d491af0dcc447dee9162aa4c159ba54e
mleitner8 <127556229+mleitner8@users.noreply.github.com>
2024-12-13 16:07:29 -0500

    Update batch.py

--- batch.py
+++ batch.py
@@ -4,22 +4,26 @@ import os
 import json
 
 sections = list(json.load(open('Na12HH16HH_TF.json', 'r'))['secs'].keys())
-weights = [0.001, 0.002]
+weights = list(np.arange(0.01, 0.2, 0.01)/100.0) 
 # Create parameter grid for search
 params = {
     'sec'   : sections,
     'weight': weights,
 }
 
-# use batch_shell_config if running directly on the machine
-shell_config = {"command": "python test.py"}
+# use batch_sge_config if running on a
+sge_config = {
+    'queue': 'cpu.q',
+    'cores': 2,
+    'vmem': '4G',
+    'realtime': '04:00:00',
+    'command': 'python test.py'}
 
-run_config = shell_config
 
-result_grid = search(job_type = 'sh',
+result_grid = search(job_type = 'sge',
        comm_type       = "socket",
        params          = params,
-       run_config      = shell_config,
+       run_config      = sge_config,
        label           = "grid_search",
        output_path     = "./grid_batch",
        checkpoint_path = "./ray",
