bd097211daab151ef4ba7a4f610e3fdaa2cea373
mleitner8 <127556229+mleitner8@users.noreply.github.com>
2024-12-13 16:07:52 -0500

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,26 +1,40 @@
 import pandas
 import ast
 from scipy.interpolate import interp1d
+import pickle
+
 
 def parse_sec_loc(sec_loc):
     return ast.literal_eval(sec_loc)
 
-EPSPNORM = 0.5
-df = pandas.read_csv('wscale_search.csv')[['config/sec_loc', 'config/weight', 'epsp']]
-sec_locs = df['config/sec_loc'].apply(parse_sec_loc)
-df[['sec', 'loc']] = pandas.DataFrame(sec_locs.tolist(), index=df.index)
-secs = df['sec'].unique()
-"""
-for sec in secs:
-    locs = df[df['sec'] == sec]['loc'].unique()
-    
-# for each section calculate the weight where the epsp at soma == 0.5
-    entries = df[df['config/sec'] == sec].sort_values(by='config/weight')
-    #print(entries)
-    weights = entries['config/weight']
-    epsps = entries['epsp']
-    f = interp1d(epsps, weights, fill_value='extrapolate')
-    #print([*zip(weights, epsps)])
-    wnorm = f(EPSPNORM) / EPSPNORM
-"""
 
+EPSPNORM = 0.5
+df = pandas.read_csv('grid_search.csv')[['config/sec', 'config/weight', 'epsp']]
+df.rename(columns={'config/sec': 'sec', 'config/weight': 'weight'}, inplace=True)
+
+secs = df['sec'].unique()
+
+weight_norms = {}
+
+
+for sec in secs:
+    # for each section calculate the weight where the epsp at soma == 0.5
+    entries = df[df['sec'] == sec].sort_values(by='weight')
+    #print(entries)
+    weights = entries['weight']
+    epsps = entries['epsp']
+
+    f = interp1d(epsps, weights, fill_value='extrapolate')
+    print(f)
+    #print([*zip(weights, epsps)])
+    #if f(EPSPNORM) < 0:
+        #f = interp1d(epsps[:-1], weights, fill_value="extrapolate")
+
+    #wnorm = f(EPSPNORM) / EPSPNORM
+    weight_norm = f(EPSPNORM) / EPSPNORM
+    weight_norms[sec] = [weight_norm]
+
+
+
+with open('weight_norms.pkl', 'wb') as fptr:
+    pickle.dump(weight_norms, fptr)
